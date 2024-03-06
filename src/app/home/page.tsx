245f8c9230c6cc85cebb7d18c0c43b88f330afbf
jeferson-viana <jefersonv28@gmail.com>
2024-03-06 02:32:09 -0300

    FEAT: add icons edit and delete

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { AppStateProvider } from "@/context/AppProvider";
+import Card from '../../components/card/Card';
 // const phone = {
 //   id: 1,
 //   name: "product 1",
@@ -19,32 +20,7 @@ export default function Home() {
         className="flex flex-col items-center justify-center w-85"
       >
       {listPhones.map((phone: any) => (
-        <div
-          className="flex flex-col justify-between my-2 w-full rounded shadow-md"
-          key={phone.id}
-        >
-          <div
-            className="bg-slate-200 w-full h-12 rounded-t-md rounded-b-none flex items-center justify-center text-slate-900 font-bold text-xl"
-          >
-            <span>{phone.name}</span>
-          </div>
-          <div
-            className="flex flex-col justify-between p-2 bg-slate-200 rounded-b-md rounded-t-none opacity-30"
-          >
-            <p
-              className="text-slate-900"
-            >{`Marca: ${phone.brand}`}</p>
-            <p
-              className="text-slate-900"
-            >{`Modelo: ${phone.model}`}</p>
-            <p
-              className="text-slate-900"
-            >{`Valor: R$${phone.price},00`}</p>
-            <p
-              className="text-slate-900"
-            >{`Cor: ${phone.color}`}</p>
-          </div>
-        </div>
+        <Card phone={phone} key={phone.id} />
       ))}
       </section>
     </div>
