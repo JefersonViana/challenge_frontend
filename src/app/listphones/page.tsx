9ff966cb0bb0bbcd64f9ae945025fce1e1d4f576
jeferson-viana <jefersonv28@gmail.com>
2024-03-06 11:52:51 -0300

    FEAT: creating search input

---
+++ src/app/listphones/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { AppStateProvider } from "@/context/AppProvider";
+import Card from '../../components/card/Card';
+import { useEffect, useState } from "react";
+import Filters from "@/components/filters/Filters";
+
+export default function ListPhones() {
+  const [list, setList] = useState<any>([]);
+  const { listPhones } = AppStateProvider();
+  
+  useEffect(() => {
+    setList(listPhones);
+  }, []);
+
+  return (
+    <div
+      className="flex flex-col items-center justify-center w-full bg-slate-900"
+    >
+      <Filters setList={setList} />
+      <section className="flex flex-col items-center justify-center w-85">
+        {list.map((phone: any) => (
+          <Card phone={phone} key={phone.id} />
+        ))}
+      </section>
+    </div>
+  );
+}
